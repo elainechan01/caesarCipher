9627f7d24725bdbedf34671f9ce998d9a36ef8f2
Elaine <71743181+elainechan01@users.noreply.github.com>
2021-07-13 00:08:20 -0400

    Add files via upload

---
+++ caesarCipher.py
@@ -0,0 +1,71 @@
+"""
+Program to implement Caesar Cipher
+"""
+
+def quit_program():
+    invalidValue = True
+    while invalidValue:
+        userInput = input("Quit? (y/n): ")
+        if userInput.lower() == "y":
+            return True
+        elif userInput.lower() == "n":
+            return False
+        else:
+            print("Invalid value.")
+
+def get_user_input():
+    userInput = input("\nEnter your string here: ")
+    return userInput
+
+def get_shift():        # CHALLENGE PORTION
+    invalidValue = True
+    while invalidValue:
+        userInput = input("Enter shift value here: ")
+        try:
+            return int(userInput)
+        except ValueError:
+            print("Invalid value.")
+
+def split_ascii(word):                  # CHALLENGE PORTION
+    return [char for char in word]
+
+def changeCharacter(char, shift, alphabet):
+    isCaps = False
+    if char.isupper():
+        isCaps = True
+        char = char.lower()
+    if char in alphabet:
+        charCurrentIndex = alphabet.index(char)
+        charNewIndex = charCurrentIndex + shift
+        while charNewIndex < 0 or charNewIndex > 25:
+            if charNewIndex > 25:
+                charNewIndex -= 26
+            elif charNewIndex < 0:
+                charNewIndex += 26
+        newChar = alphabet[charNewIndex]
+        if isCaps:
+            newChar = newChar.upper()
+        return newChar
+    else:
+        return char
+
+def main():
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet_list = split_ascii(alphabet)
+    caesarCode = []
+    quit = False
+    while not quit:
+        string = get_user_input()
+        shift = get_shift()
+        for char in string:
+            newChar = changeCharacter(char, shift, alphabet_list)
+            caesarCode.append(newChar)
+        print("\nThe encoded message is: ")
+        print(''.join(map(str, caesarCode)))
+        print()
+        caesarCode.clear()
+        quit = quit_program()
+
+
+if __name__ == "__main__":
+    main()
